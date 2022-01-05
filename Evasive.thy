d352b4f31a9b0d3cdd8d65eb97c2848853af2679
Julius Michaelis <glitter@liftm.de>
2022-01-06 00:53:38 +0900

    Alternative definition of evasiveness

--- Evasive.thy
+++ Evasive.thy
@@ -217,33 +217,152 @@ text\<open>Now we introduce the definition of evasive Boolean function.
 definition evasive :: "nat => (bool vec => bool) => bool"
   where "evasive n f \<equiv> (height (mk_ifex (vec_to_boolfunc n f) [0..<n])) = n"
 
+(* all right, that can't be it. *)
+(* First, consider the non-evasive function "if a2 then a1 else a3" *)
+(* Here's a BDT for it: *)
+lemma "val_ifex (IF 2 (IF 1 Trueif Falseif) (IF 3 Trueif Falseif)) = (\<lambda>a. if a 2 then a 1 else a 3)"
+  by (rule ext; simp)
+(* problem? height \<circ> mk_ifex on this returns 3 *)
+value "mk_ifex (\<lambda>a. if a 2 then a 1 else a 3) [3,2,1::nat]"
+lemma "height (mk_ifex (\<lambda>a. if a 2 then a 1 else a 3) [2,3,1::nat]) = 3"
+  by eval
+(* Note that the order of the variables in the list argument does not affect the output of mk_ifex*)
+lemma "reads_inside_set f (set v2) \<Longrightarrow> set v1 = set v2 \<Longrightarrow> mk_ifex f v1 = mk_ifex f v2"
+  (* This actually holds without the reads_inside_set, but the argument is a lot more subtle.. *)
+  by (simp add: mk_ifex_ro ro_ifex_unique val_ifex_mk_ifex_equal)
+(* but in fact, the order defined on the type of the atoms (nat here) decides all. *)
+(* Why am I talking about this? Because mk_ifex returns reduced, ordered BDTs, and all BDDs must be reduced and ordered,
+   and thus have the same height. (I won't prove the thus part, but I'm sure. I'm not a 100% sure all BDDs must be reduced/ordered) *)
+(* Now for the sad part: There are situations where reduced ordered BDTs cannot have optimal height for any variable ordering *)
+definition "mieser \<equiv> (\<lambda>a. if a 0 then (if a 2 then a 1 else a 3) 
+                                 else (if a 1 then a 2 else a 3)) :: nat boolfunc"
+definition "Mieser \<equiv> (IF 0 (IF 2 (IF 1 Trueif Falseif) (IF 3 Trueif Falseif)) 
+                           (IF 1 (IF 2 Trueif Falseif) (IF 3 Trueif Falseif))) :: nat ifex"
+lemma "val_ifex Mieser = mieser" by(rule ext; simp add: Mieser_def mieser_def)
+(* Our manually constructed optimal BDT has height 3 *)
+lemma "height Mieser = 3" by(simp add: Mieser_def)
+(* But be careful: This lemma might read to you as "any reduced ordered bdt has height 4"
+   But this is only for the one fixed variable order given by nats
+   You'll have to manually convince yourself that this holds true even if e.g. 2 < 1.
+ *)
+lemma "ro_ifex f \<Longrightarrow> val_ifex f = mieser \<Longrightarrow> height f = 4"
+(* In fact, this lemma is trivial. There is only one ro bdt that expresses mieser for the given order of nats *)
+proof -
+  have set: "reads_inside_set mieser (set [0,1,2,3])" by(simp add: mieser_def reads_inside_set_def)
+  have 4: "height (mk_ifex mieser [0,1,2,3]) = 4" by eval
+  assume m: "val_ifex f = mieser"
+  assume "ro_ifex f"
+  from ro_ifex_unique[OF this mk_ifex_ro, unfolded m] val_ifex_mk_ifex_equal[OF set]
+  have "f = mk_ifex mieser [0,1,2,3]" by meson
+  thus ?thesis using 4 by meson
+qed
 
-primrec ifex_reduce where
-"ifex_reduce Falseif = Falseif" |
-"ifex_reduce Trueif = Trueif" |
-"ifex_reduce (IF v t e) = (let t = ifex_reduce t; e = ifex_reduce e in (if t = e then t else IF v t e))"
-lemma ifex_reduce[simp,intro!]: "ifex_minimal (ifex_reduce f)"
-  by(induction f; simp)
-lemma ifex_reduce_varset: "ifex_var_set (ifex_reduce f) \<subseteq> ifex_var_set f"
+
+definition bf_evasive_on :: "'a boolfunc \<Rightarrow> 'a set \<Rightarrow> bool"
+  where "bf_evasive_on f vs \<equiv> (\<exists>assmt. \<forall>update \<in> vs. f assmt \<noteq> f (assmt(update := \<not>assmt update)))"
+
+definition "all_bdts f vars = {t. ifex_var_set t \<subseteq> vars \<and> val_ifex t = f}"
+(* sanity checks\<dots> *)
+lemma "Falseif \<in> all_bdts (\<lambda>_. False) x"
+  unfolding all_bdts_def
+  by auto
+lemma "Falseif \<notin> all_bdts (\<lambda>a. a b) x"
+  unfolding all_bdts_def
+  by (auto dest: fun_cong[where x="\<lambda>_. True"])
+lemma "IF 0 Trueif Falseif \<in> all_bdts (\<lambda>a. a 0) ({0} \<union> x)"
+  by(simp add: all_bdts_def; rule ext; simp)
+
+definition "bf_blind f vars \<equiv> \<lambda>assmt. f (\<lambda>v. if v \<in> vars then assmt v else False)"
+lemma "reads_inside_set f vs \<Longrightarrow> bf_blind f vs = f"
+  unfolding bf_blind_def
+  unfolding reads_inside_set_def
+  by force
+  (* guess I don't need bf_blind *)
+
+(* This thing is unusable *)
+definition "bf_significant_vars f \<equiv> \<Inter>{vars. reads_inside_set f vars}"
+
+(* truth table. less smart than mk_ifex, easier to work with. *)
+fun mk_ifex_tt :: "('a :: linorder) boolfunc \<Rightarrow> 'a list \<Rightarrow> 'a ifex" where
+"mk_ifex_tt f [] = (if f (const False) then Trueif else Falseif)" |
+"mk_ifex_tt f (v#vs) = (IF v
+                      (mk_ifex_tt (bf_restrict v True f) vs)
+                      (mk_ifex_tt (bf_restrict v False f) vs))"
+
+lemma mk_ifex_tt_varset[simp]: "ifex_var_set (mk_ifex_tt f vs) = set vs"
+  by(induction vs arbitrary: f; simp)
+lemma mk_ifex_tt_height[simp]: "height (mk_ifex_tt f vs) = length vs"
+  by(induction vs arbitrary: f; simp)
+
+lemma mk_ifex_tt[simp]: "reads_inside_set f (set vs) \<Longrightarrow> val_ifex (mk_ifex_tt f vs) = f"
+proof(induction vs arbitrary: f)
+  case Nil
+  then show ?case by(auto dest!: reads_none)
+next
+  case (Cons a vs)
+  have "reads_inside_set (bf_restrict a t f) (set vs)" for t 
+    by (metis Cons.prems member_remove reads_inside_set_restrict reads_inside_set_subset removeAll.simps(2) remove_code(1) subset_code(1))
+  from Cons.IH[OF this]
+  show ?case by (simp add: bf_restrict_def fun_upd_idem)
+qed
+
+lemma mk_ifex_tt_all_bdts: "reads_inside_set f (set vs) \<Longrightarrow> mk_ifex_tt f vs \<in> all_bdts f (set vs)"
+  unfolding all_bdts_def by simp
+
+definition "bf_evasive_on_alt f vs \<equiv> (let abh = height ` all_bdts f vs; c = card vs in c \<in> abh \<and> (\<forall>h \<in> abh. c \<le> h))"
+(* Using Min makes things weird... *)
+find_theorems "Min (_ :: nat set)" (* like, why are there so few facts on it? *)
+
+fun ifex_vartrail where
+"ifex_vartrail (IF v t e) a = v # ifex_vartrail (if a v then t else e) a" |
+"ifex_vartrail _ a = []"
+lemma ifex_vartrail_length_height: "length (ifex_vartrail f as) \<le> height f"
   by(induction f) auto
-lemma ifex_reduce_ordered[intro!]: "ifex_ordered f \<Longrightarrow> ifex_ordered (ifex_reduce f)"
-  using ifex_reduce_varset by(induction f) auto
-lemma ifex_reduce_val[simp]: "val_ifex (ifex_reduce f) = val_ifex f"
+lemma ifex_vartrail: "p \<notin> set (ifex_vartrail f as) \<Longrightarrow> val_ifex f (as(p:=t)) = val_ifex f as"
+  by(induction f) auto
+lemma ifex_vartrail_subset: "set (ifex_vartrail f as) \<subseteq> ifex_var_set f"
   by(induction f) auto
 
-definition "ifex_rename from to f \<equiv> ifex_ite (IF to Trueif Falseif) (ifex_reduce (restrict f from True)) (ifex_reduce (restrict f from False))" 
-lemma rename_removes: "a \<noteq> b \<Longrightarrow> a \<notin> ifex_var_set (ifex_rename a b f)"
-  apply (auto dest!: ifex_vars_subset simp del: ifex_ite.simps simp add: not_element_restrict ifex_rename_def)
+lemma bf_evasive_alt:
+  assumes fin: "finite vs" 
+  assumes rin: "reads_inside_set f vs"
+  shows "bf_evasive_on f vs \<longleftrightarrow> bf_evasive_on_alt f vs"
+proof
+  assume "bf_evasive_on f vs"
+  then obtain a where a: "\<And>p. p \<in> vs \<Longrightarrow> f a \<noteq> f (a(p := \<not>a p))" 
+    unfolding bf_evasive_on_def by blast
+
+  obtain lvs where lvs: "set lvs = vs" "length lvs = card vs"
+    by (metis fin finite_sorted_distinct_unique length_remdups_card_conv set_remdups)
+  with mk_ifex_tt_all_bdts rin have "height (mk_ifex_tt f lvs) \<in> {height t |t. t \<in> all_bdts f vs}"
+    by blast
+  hence isin: "card vs \<in> height ` all_bdts f vs" 
+    by(auto simp add: image_iff lvs)
+
+  have ismin: "\<forall>t \<in> all_bdts f vs. card vs \<le> height t"
+  proof(rule ccontr)
+    assume "\<not> (\<forall>t\<in>all_bdts f vs. card vs \<le> height t)"
+    then obtain t where t: "t \<in> all_bdts f vs" "height t < card vs" by force
+    hence "length (ifex_vartrail t a) < card vs"
+      by (meson ifex_vartrail_length_height le_trans less_le_not_le)
+    then obtain p where p: "p \<notin> set (ifex_vartrail t a)" "p \<in> vs"
+      by (meson List.finite_set card_length card_mono le_trans linorder_not_le subsetI)
+    with a have up: "f a \<noteq> f (a(p := \<not>a p))" by presburger
+    have "val_ifex t = f" using t unfolding all_bdts_def by simp
+    with ifex_vartrail[OF p(1)] up
+    show False by simp
+  qed
+    
+  from isin ismin show "bf_evasive_on_alt f vs" 
+    unfolding bf_evasive_on_alt_def Let_def
+    by simp
+next
+  assume rhs: "bf_evasive_on_alt f vs"
+  from rhs have ismin: "\<forall>t \<in> all_bdts f vs. card vs \<le> height t" unfolding bf_evasive_on_alt_def by simp
+  from rhs have isin: "\<exists>x\<in>all_bdts f vs. card vs = height x" unfolding bf_evasive_on_alt_def by auto
+  show "bf_evasive_on f vs"
   oops
-
-lemma val_ifex_rename: "ifex_ordered f \<Longrightarrow> val_ifex (ifex_rename a b f) assmt = val_ifex f (assmt(a := assmt b))"
-  apply (clarsimp simp del: ifex_ite.simps simp add: not_element_restrict ifex_rename_def)
-  apply(subst val_ifex_ite_subst)
-     apply(simp;fail)
-    apply (simp add: ifex_reduce_ordered restrict_ifex_ordered_invar; fail)
-   apply (simp add: ifex_reduce_ordered restrict_ifex_ordered_invar; fail)
-  apply(simp add: bf_ite_def restrict_assignment[symmetric])
-  done
+  
 
 
 (*definition evasive :: "nat => ((nat => bool) => bool) => bool"
